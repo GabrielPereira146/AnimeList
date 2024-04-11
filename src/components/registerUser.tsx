62207f8f4e9f0601514eb29e74d2327922f9e940
GabrielPereira146 <gabriel.brown1112@gmail.com>
2024-04-11 09:18:48 -0300

    desing complete

--- src/components/registerUser.tsx
+++ src/components/registerUser.tsx
@@ -1,5 +1,12 @@
+import { LockIcon, MailIcon, User2 } from "lucide-react";
 
 export function RegisterUser() {
+
+    function validatePassword(confsenha:string, senha:string){
+
+    }
+
+
     return (
         <div className="size-full flex px-64 py-24">
             <div className='flex flex-row size-full bg-white/10 border-white/35 rounded-xl backdrop-blur-md'>
@@ -10,12 +17,28 @@ export function RegisterUser() {
                 </div>
                 <div className='flex flex-col items-center size-full px-20 py-32 gap-5 '>
                     <span className="text-4xl font-medium text-center drop-shadow-lg">CADASTRE-SE</span>
-                    <div className=" shadow-xl rounded-full w-96 h-12">
-                        <input className=" border-transparent bg-white/35 rounded-full w-96 h-12" type="text" value={"USUARIO"} />
+                    <div className="flex flex-row items-center shadow-xl rounded-full pl-3 bg-white/35 w-96 h-12">
+                        <User2 className="size-9"></User2>
+                        <input className="focus:ring-transparent focus:border-transparent border-transparent bg-transparent rounded-e-full w-full h-12 font-semibold text-2xl placeholder-pink-800/60 text-pink-800/60" type="text" placeholder="USUARIO" />
                     </div>
+                    <div className="flex flex-row items-center shadow-xl rounded-full pl-3 bg-white/35 w-96 h-12">
+                        <MailIcon className="size-9"></MailIcon>
+                        <input className="focus:ring-transparent focus:border-transparent border-transparent bg-transparent rounded-e-full w-full h-12 font-semibold text-2xl placeholder-pink-800/60 text-pink-800/60" type="email" placeholder="EMAIL" />
+                    </div>
+                    <div className="flex flex-row items-center shadow-xl rounded-full pl-3 bg-white/35 w-96 h-12">
+                        <LockIcon className="size-9"></LockIcon>
+                        <input className=" focus:ring-transparent focus:border-transparent border-transparent bg-transparent rounded-e-full w-full h-12 font-semibold text-2xl placeholder-pink-800/60 text-pink-800/60" type="password" placeholder="SENHA" />
+                    </div>
+                    <div className="flex flex-row items-center shadow-xl rounded-full pl-3 bg-white/35 w-96 h-12">
+                        <LockIcon className="size-9"></LockIcon>
+                        <input className=" focus:ring-transparent focus:border-transparent border-transparent bg-transparent rounded-e-full w-full h-12 font-semibold text-2xl placeholder-pink-800/60 text-pink-800/60" type="password" placeholder="CONFIRMAR SENHA" />
+                    </div>
+                    <div className="flex items-center">
+                        <input type="checkbox" className="border-zinc-700/10 bg-white/35 rounded-sm focus:ring-transparent text-pink-800/60" name="Lembrar" id="lembre" />
+                        <label className="ms-2 text-base font-medium text-pink-900">Lembrar de mim</label>
+                    </div>
+                    <button className='bg-pink-900 rounded-full w-72 h-16 text-3xl shadow-xl text-white '>CADASTRAR</button>
 
-                    <input className="bg-white/35 shadow-xl rounded-full w-96 h-12 border-transparent" type="text" />
-                    <input className="bg-white/35 shadow-xl rounded-full w-96 h-12 border-transparent" type="text" />
                 </div>
             </div>
         </div>

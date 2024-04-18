ef1051b738f49e1b5104a56b0bfc51a56e1ed655
GabrielPereira146 <gabriel.brown1112@gmail.com>
2024-04-18 14:26:23 -0300

    config routes

--- src/main.tsx
+++ src/main.tsx
@@ -13,31 +13,28 @@ import Home from './routes/home';
 
 const router = createBrowserRouter([
   {
-
+    
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
     children: [
 
       {
-        path: "",
-        element: <RegisterLoginUser />,
-      },
-      {
-        path: "home/",
-        element: < Home />,
-      },
-      {
         path: "animes/",
         element: <Animes />,
       },
-
-
+      {
+        path: "home/",
+        element: < Home/>,
+      },
     ],
   },
+  {
+    path: "/login",
+    element: <RegisterLoginUser />,
+  },
 
-
-
+  
 
 ]);
 

123e0c4356d5ce917754d59bf6b3278217a07881
GabrielPereira146 <gabriel.brown1112@gmail.com>
2024-05-06 21:09:06 -0300

    feature show top 7 works

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,4 @@
 import { Genre } from "./genre";
-import { ChevronLeft, ChevronRight } from 'lucide-react';
 
 interface HeroProps {
     titleExibition: string;
@@ -14,12 +13,12 @@ export function Hero({titleExibition,title,synopsis,average,author,cover}: HeroP
 
     return (
         <div className="relative bg-no-repeat bg-cover bg-top z-0" style={{ backgroundImage: `linear-gradient(to bottom, rgba(255, 255, 255, 0), rgba(255, 255, 255, 1)), url(${cover})` }}>
-                <div className="flex flex-col px-6 py-6 gap-2 ">
-                    <span className="text-2xl font-bold text-zinc-50 ">{titleExibition}</span>
+                <div className="flex flex-col px-6 py-6 gap-2 bg-gradient-to-b from-black">
+                    <span className="text-2xl font-bold text-zinc-200 ">{titleExibition}</span>
                     <div className="size-auto flex flex-row gap-3 py-5">
                         <img className="w-56 h-80 rounded-lg" src={cover} alt="Anime Cover" />
                         <div className="size-full flex flex-col gap-4 px-3 ">
-                            <span className="text-4xl font-medium text-zinc-50 drop-shadow-sm">{title}</span>
+                            <span className="text-4xl font-medium text-zinc-200 drop-shadow-sm">{title}</span>
                             <div className="flex flex-row gap-1">
                                 <Genre>Shounen</Genre>
                                 <Genre>Action</Genre>
@@ -27,7 +26,7 @@ export function Hero({titleExibition,title,synopsis,average,author,cover}: HeroP
                                 <Genre>Martial Arts</Genre>
                             </div>
                             <div className="h-36 max-w-max">
-                                <span className=" h-36 text-lg text-zinc-950 font-semibold line-clamp-6 drop-shadow-2xl">{synopsis}</span>
+                                <span className=" h-36 text-lg text-zinc-200 font-semibold line-clamp-6 drop-shadow-2xl">{synopsis}</span>
                             </div>
                             <span className="text-2xl font-semibold text-zinc-800 ">Nota: {average}</span>
                             <span className="text-base font-bold text-zinc-800 ">{author}</span>
--- src/hooks/useWorkData.tsx
+++ src/hooks/useWorkData.tsx
@@ -3,15 +3,20 @@ import { WorkData } from "../interface/WorkData";
 import { useQuery } from "@tanstack/react-query";
 const API_URL = 'http://localhost:8080';
 
-const fecthData = async (): AxiosPromise<WorkData[]> => {
+const fetchAllWorks = async (): AxiosPromise<WorkData[]> => {
     const response = axios.get(API_URL+'/works/');
+    return response;
+}
+
+const fetchTopWorks = async (): AxiosPromise<WorkData[]> => {
+    const response = axios.get(API_URL+'/works/bestWorks');
     return response;
 }
 
 export function useWorkData() {
     
     const query = useQuery({
-        queryFn: fecthData,
+        queryFn: fetchTopWorks,
         queryKey: ['work-data'],
         retry: 2
     })
